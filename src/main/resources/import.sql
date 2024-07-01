480ab3ff89a65dcba565817ec8d5bf6da2d8e61f
ItalianJackWEIRD <jackybox93@gmail.com>
2024-07-01 17:49:38 +0200

    Inizio front end,
    Manca solo l'autorizzazione e poi sito finito

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,8 @@
-insert into struttura (id, name, city, url_Image) values(nextval('struttura_seq'), 'Bosco Verticale', 'Milano', '/images/bosco-verticale-milano.png');
+insert into struttura (id, name, city, url_Image, host_id) values(nextval('struttura_seq'), 'Bosco Verticale', 'Milano', '/images/bosco-verticale-milano.png', '51');
+insert into struttura (id, name, city, url_Image, host_id) values(nextval('struttura_seq'), 'Villa Marta', 'Roma', '/images/villa-marta.png', '51');
+insert into struttura (id, name, city, url_Image, host_id) values(nextval('struttura_seq'), 'Villa Este', 'Tivoli', '/images/villa-deste.png', '51');
+insert into struttura (id, name, city, url_Image, host_id) values(nextval('struttura_seq'), 'West Hollywood Oasis', 'Los Angeles', '/images/west-hollywood-oasis.png', '1');
+insert into struttura (id, name, city, url_Image, host_id) values(nextval('struttura_seq'), 'Casa Bianca', 'Washington', '/images/casa-bianca.png', '101');
+insert into host(id, name, surname, url_Image) values(nextval('host_seq'), 'John', 'Cena', '/images/hostJohnCena.png')
+insert into host(id, name, surname, url_Image) values(nextval('host_seq'), 'Lorenzo', 'Pellegrini', '/images/hostLorenzoPellegrini.png')
+insert into host(id, name, surname, url_Image) values(nextval('host_seq'), 'John Fitzgerald', 'Kennedy', '/images/hostJohnFitzgeraldKennedy.png')
