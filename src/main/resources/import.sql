7a88aee802090ad6d9f8596b8c18395c267fb43a
allozziotharo <01fgermano@gmail.com>
2024-07-12 20:55:28 +0200

    SITO COMPLETATO E AGGIUNTA IMMAGINI DA FILE SYSTEM
    DB inizialmente vuoto, verrà popolato solamente dagli utenti registrati, senza nessuna tipologia di Admin

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,19 +1,11 @@
-insert into host(name, surname) values('Gino', 'Forti');
-insert into host(name, surname) values('Sandro', 'Reni');
-insert into host(name, surname) values('Claudio', 'Campioni');
-insert into host(name, surname) values('Achille', 'Lauro');
+/*insert into host(name, surname) values('Achille', 'Lauro');*/
+/*insert into host(name, surname) values('Gino', 'Forti');*/
+/*insert into host(name, surname) values('Sandro', 'Reni');*/
+/*insert into host(name, surname) values('Claudio', 'Campioni');*/
 
-
-insert into struttura (name, city, host_id) values('West Hollywood Oasis', 'Los Angeles', '1');
-insert into struttura (name, city, host_id) values('Bosco Verticale', 'Milano', '2');
-insert into struttura (name, city, host_id) values('Villa Marta', 'Roma', '2');
-insert into struttura (name, city, host_id) values('Villa Este', 'Tivoli', '2');
-insert into struttura (name, city, host_id) values('Casa Bianca', 'Washington', '3');
-insert into struttura (name, city, host_id) values('A Casa De Sandro', 'Roma', '4');
-
-
-
-
-
-
-
+/*insert into struttura (name, city, host_id) values('A Casa De Sandro', 'Roma', '1');*/
+/*insert into struttura (name, city, host_id) values('West Hollywood Oasis', 'Los Angeles', '2');*/
+/*insert into struttura (name, city, host_id) values('Bosco Verticale', 'Milano', '3');*/
+/*insert into struttura (name, city, host_id) values('Villa Marta', 'Roma', '3');*/
+/*insert into struttura (name, city, host_id) values('Villa Este', 'Tivoli', '3');*/
+/*insert into struttura (name, city, host_id) values('Casa Bianca', 'Washington', '4');*/
