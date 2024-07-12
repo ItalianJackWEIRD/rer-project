9d757d1c4ee3c6f66b369aa7fef211232ee8add8
allozziotharo <01fgermano@gmail.com>
2024-07-12 13:29:30 +0200

    tentativo import immagini con insert statements
    TODO: questo era un commit di checkpoint per iniziare a provare con jdbc template

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,19 +1,34 @@
-insert into host(name, surname) values('John', 'Cena');
-insert into host(name, surname) values('Lorenzo', 'Pellegrini');
-insert into host(name, surname) values('John Fitzgerald', 'Kennedy');
+insert into host(name, surname) values('Gino', 'Forti');
+insert into host(name, surname) values('Sandro', 'Reni');
+insert into host(name, surname) values('Claudio', 'Campioni');
+insert into host(name, surname) values('Achille', 'Lauro');
+
+
+insert into struttura (name, city, host_id) values('West Hollywood Oasis', 'Los Angeles', '1');
 insert into struttura (name, city, host_id) values('Bosco Verticale', 'Milano', '2');
 insert into struttura (name, city, host_id) values('Villa Marta', 'Roma', '2');
 insert into struttura (name, city, host_id) values('Villa Este', 'Tivoli', '2');
-insert into struttura (name, city, host_id) values('West Hollywood Oasis', 'Los Angeles', '1');
 insert into struttura (name, city, host_id) values('Casa Bianca', 'Washington', '3');
+insert into struttura (name, city, host_id) values('A Casa De Sandro', 'Roma', '4');
+
 
 /*immagini hosts*/
-insert into immagine(file_name, image_data) values ('hostJohnCena', LOAD_FILE('images\hostJohnCena.png'));
-insert into immagine(file_name, image_data) values ('hostJohnFitzgeraldKennedy', LOAD_FILE('images\hostJohnFitzgeraldKennedy.png'));
-insert into immagine(file_name, image_data) values ('hostLorenzoPellegrini', LOAD_FILE('images\hostLorenzoPellegrini.png'));
-insert into immagine(file_name, image_data) values ('hostAchilleLauro', LOAD_FILE('images\hostAchilleLauro.png'));
+INSERT INTO immagine (file_name, image_data) VALUES ('hostGinoForti', pg_read_binary_file('C:\Users\01fge\Documents\GitHub\rer-project\src\main\resources\static\images\hostGinoForti.png'));
+INSERT INTO immagine (file_name, image_data) VALUES ('hostSandroReni', pg_read_binary_file('C:\Users\01fge\Documents\GitHub\rer-project\src\main\resources\static\images\hostSandroReni.png'));
+INSERT INTO immagine (file_name, image_data) VALUES ('hostClaudioCampioni', pg_read_binary_file('C:\Users\01fge\Documents\GitHub\rer-project\src\main\resources\static\images\hostClaudioCampioni.png'));
+INSERT INTO immagine (file_name, image_data) VALUES ('hostAchilleLauro', pg_read_binary_file('C:\Users\01fge\Documents\GitHub\rer-project\src\main\resources\static\images\hostAchilleLauro.png'));
 
 
 /*immagini strutture*/
-insert into immagine(file_name, image_data) values ('villa-marta', LOAD_FILE('images\villa-marta.png'));
-insert into immagine(file_name, image_data) values ('villa-deste', LOAD_FILE('images\villa-deste.png'));
+INSERT INTO immagine (file_name, image_data) VALUES ('west-hollywood-oasis', pg_read_binary_file('C:\Users\01fge\Documents\GitHub\rer-project\src\main\resources\static\images\west-hollywood-oasis.png'));
+INSERT INTO immagine (file_name, image_data) VALUES ('bosco-verticale-milano', pg_read_binary_file('C:\Users\01fge\Documents\GitHub\rer-project\src\main\resources\static\images\bosco-verticale-milano.png'));
+INSERT INTO immagine (file_name, image_data) VALUES ('villa-marta', pg_read_binary_file('C:\Users\01fge\Documents\GitHub\rer-project\src\main\resources\static\images\villa-marta.png'));
+INSERT INTO immagine (file_name, image_data) VALUES ('villa-deste', pg_read_binary_file('C:\Users\01fge\Documents\GitHub\rer-project\src\main\resources\static\images\villa-deste.png'));
+INSERT INTO immagine (file_name, image_data) VALUES ('casa-bianca', pg_read_binary_file('C:\Users\01fge\Documents\GitHub\rer-project\src\main\resources\static\images\casa-bianca.png'));
+INSERT INTO immagine (file_name, image_data) VALUES ('casa-de-sandro', pg_read_binary_file('C:\Users\01fge\Documents\GitHub\rer-project\src\main\resources\static\images\casa-de-sandro.png'));
+
+
+
+
+
+
